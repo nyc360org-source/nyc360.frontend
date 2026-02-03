e9d0767638905d5c2689deb11ab749eba80ed65e
Mohamed Gaber <63926336+DevMohamedGaber@users.noreply.github.com>
2026-02-03 13:04:39 +0200

    feat: Implement housing request status display and refine agent request form with improved date/time handling.

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://nyc360.runasp.net/api',
-  apiBaseUrl2: 'https://nyc360.runasp.net',
-  apiBaseUrl3: 'https://nyc360.runasp.net/posts',
+  apiBaseUrl: 'http://localhost:5251/api',
+  apiBaseUrl2: 'http://localhost:5251/',
+  apiBaseUrl3: 'http://localhost:5251/posts',
 
   googleClientId: "498969207883-os0gf7vs10g5rcekdlo6rtd316ubatqc.apps.googleusercontent.com"
 
